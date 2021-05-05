afa608b0b9fba54d7a2a35d738e4d8873ffa971d
Tristen <tjhm9c@mail.missouri.edu>
2021-05-04 19:22:24 -0500

    RTD work

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,8 @@
 
 import os
 import sys
-from unittest import mock
 
 sys.path.insert(0, os.path.abspath('../src/'))
-MOCK_MODULES = ["mariadb", "psycopg", "google-bigquery", "pandas-gbq", "pymysql"]
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
